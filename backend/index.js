390150d41ee4c4ba4687b7b14f46092f973863c7
Ricky Z <rickyzhang2013@hotmail.com>
2024-11-28 04:55:51 +1100

    removed the old testing public/index.html page (idk how i missed that oops)

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,6 @@ const { mergeVideoAudio, downloadYtVideo, cleanUp } = require('./util.js');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080;
 
